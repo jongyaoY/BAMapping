061bdb135bb5eb7603d022ff621f2c48347e95e5
Karsten Mueller <karsten.mueller@kit.edu>
2019-10-29 17:45:04 +0100

    added BundleAjuster

---
+++ debug/CMakeFiles/BAMapping.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/BAMapping/src/Reader.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/Reader.cpp.o"
+  "/home/user/BAMapping/src/Viewer.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/Viewer.cpp.o"
+  "/home/user/BAMapping/src/util/BundleAdjuster.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/util/BundleAdjuster.cpp.o"
+  "/home/user/BAMapping/src/util/Frame.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/util/Frame.cpp.o"
+  "/home/user/BAMapping/src/util/Optimize.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/util/Optimize.cpp.o"
+  "/home/user/BAMapping/src/util/Point.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/util/Point.cpp.o"
+  "/home/user/BAMapping/src/util/bal_problem.cpp" "/home/user/BAMapping/debug/CMakeFiles/BAMapping.dir/src/util/bal_problem.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_GFLAGS_NAMESPACE=google"
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/suitesparse"
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  "/home/user/Libs/Pangolin/include"
+  "/home/user/Libs/Pangolin/build/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
