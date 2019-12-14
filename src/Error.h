0f980e9fbc7319f202c5bd4c9dcd49b2f4f7655a
JoJo-Create <maschinenbauer@163.com>
2019-12-14 19:25:05 +0100

    added new error function

--- src/Error.h
+++ src/Error.h
@@ -7,10 +7,68 @@
 
 #include "ceres/ceres.h"
 #include "ceres/rotation.h"
+struct ReprojectionError_2D {
+    ReprojectionError_2D(double observed_x, double observed_y)
+            : observed_x(observed_x), observed_y(observed_y) {}
+
+    template <typename T>
+    bool operator()(const T* const camera,
+                    const T* const point,
+                    T* residuals) const {
+        // camera[0,1,2] are the angle-axis rotation.
+        T p[3];
+        ceres::AngleAxisRotatePoint(camera, point, p);
+
+        // camera[3,4,5] are the translation.
+        p[0] += camera[3];
+        p[1] += camera[4];
+        p[2] += camera[5];
+
+        const T xp =  p[0] / p[2];
+        const T yp =  p[1] / p[2];
+
+
+        // Compute final projected point position.
+        const T& focal = camera[6];
+        const T predicted_x = fx_ * xp + cx_;
+        const T predicted_y = fy_ * yp + cy_;
+
+        // The error is the difference between the predicted and observed position.
+        residuals[0] = predicted_x - observed_x;
+        residuals[1] = predicted_y - observed_y;
+
+        return true;
+    }
+
+    // Factory to hide the construction of the CostFunction object from
+    // the client code.
+    static ceres::CostFunction* Create(const double observed_x,
+                                       const double observed_y) {
+        return (new ceres::AutoDiffCostFunction<ReprojectionError_2D, 2, 6, 3>(
+                new ReprojectionError_2D(observed_x, observed_y)));
+    }
+    static void setIntrinsics(double fx,double fy,double cx,double cy)
+    {
+        fx_ = fx;
+        fy_ = fy;
+        cx_ = cx;
+        cy_ = cy;
+    }
+    static double fx_;
+    static double fy_;
+    static double cx_;
+    static double cy_;
+
+    double observed_x;
+    double observed_y;
+};
+
+
+
 struct AlignmentError_3D
 {
-    AlignmentError_3D(double observed_x, double observed_y, double observed_z)
-            : u(observed_x), v(observed_y), d(observed_z) {}
+    AlignmentError_3D(double observed_u, double observed_v, double observed_d)
+            : u(observed_u), v(observed_v), d(observed_d) {}
 
     template <typename T>
     bool operator()(const T* const camera,
@@ -30,18 +88,27 @@ struct AlignmentError_3D
         p_obs[1] = (T) (v-cy_)*d/fy_;
         p_obs[2] = (T) d;
 
+        if(d<=(T) 3.0)
+        {
+            residuals[0] = p[0] - p_obs[0];
+            residuals[1] = p[1] - p_obs[1];
+            residuals[2] = p[2] - p_obs[2];
+        }
+        else
+        {
+            residuals[0] = (T) 0.;
+            residuals[1] = (T) 0.;
+            residuals[2] = (T) 0.;
 
-        residuals[0] = p[0] - p_obs[0];
-        residuals[1] = p[1] - p_obs[1];
-        residuals[2] = p[2] - p_obs[2];
+        }
         return true;
     }
 
-    static ceres::CostFunction* Create(const double observed_x,
-                                       const double observed_y,
-                                       const double observed_z) {
+    static ceres::CostFunction* Create(const double observed_u,
+                                       const double observed_v,
+                                       const double observed_d) {
         return (new ceres::AutoDiffCostFunction<AlignmentError_3D, 3, 6, 3>(
-                new AlignmentError_3D(observed_x, observed_y,observed_z)));
+                new AlignmentError_3D(observed_u, observed_v,observed_d)));
     }
 
     double u;
@@ -109,5 +176,9 @@ double AlignmentError_3D::fy_;
 double AlignmentError_3D::cx_;
 double AlignmentError_3D::cy_;
 
+double ReprojectionError_2D::fx_;
+double ReprojectionError_2D::fy_;
+double ReprojectionError_2D::cx_;
+double ReprojectionError_2D::cy_;
 
 #endif //BAMAPPING_ERROR_H
